b45bb69cb0e5cc5d2d60289f65a3296a336bc0d2
Alexander Peresypkin <peresypkin10@gmail.com>
2018-12-12 01:01:10 +0300

    Initial Commit

---
+++ CryptoCoins/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CryptoCoins
+//
+//  Created by Alexander Peresypkin on 12/12/2018.
+//  Copyright © 2018 Alexander Peresypkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
