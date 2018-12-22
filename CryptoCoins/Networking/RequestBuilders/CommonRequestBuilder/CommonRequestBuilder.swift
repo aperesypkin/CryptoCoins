0f976fcfdea269676bfc011a5a44ff5ed25553aa
Alexander Peresypkin <peresypkin10@gmail.com>
2018-12-22 15:41:39 +0300

    Реализовал RequestClient

---
+++ CryptoCoins/Networking/RequestBuilders/CommonRequestBuilder/CommonRequestBuilder.swift
@@ -0,0 +1,37 @@
+//
+//  CommonRequestBuilder.swift
+//  Debug
+//
+//  Created by Alexander Peresypkin on 22/12/2018.
+//  Copyright © 2018 Alexander Peresypkin. All rights reserved.
+//
+
+import Foundation
+
+class CommonRequestBuilder: IRequestBuilder {
+    func buildURLRequest(from request: IRequest) -> URLRequest? {
+        var urlComponents = URLComponents(string: "\(request.domain)/\(request.apiVersion)/\(request.endpoint)")
+        
+        if let body = request.body, !body.isEmpty {
+            urlComponents?.queryItems = body.queryItems
+        }
+        
+        guard let url = urlComponents?.url else { return nil }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Accept")
+        urlRequest.addValue("deflate, gzip", forHTTPHeaderField: "Accept-Encoding")
+        urlRequest.addValue(request.apiKey, forHTTPHeaderField: "X-CMC_PRO_API_KEY")
+        return urlRequest
+    }
+}
+
+extension Dictionary where Key == String, Value == String {
+    var queryItems: [URLQueryItem] {
+        var items: [URLQueryItem] = []
+        for (key, value) in self {
+            items.append(URLQueryItem(name: key, value: value))
+        }
+        return items
+    }
+}
